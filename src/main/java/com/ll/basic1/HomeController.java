f79712852cc29bf5bfd603f0875408f71926bd8b
Y39 <y39scode@gmail.com>
2023-03-14 11:44:41 +0900

    전역 변수를 이용한 count

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -7,8 +7,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 public class HomeController {
+    int count=0;
     @GetMapping("/home/main")
     public String showMain(){
         return "Hi dear!!";
     }
+
+    @GetMapping("/home/main2")
+    public String showMain2(){
+        return "Googbye dear ~";
+    }
+
+    @GetMapping("/home/main3")
+    public String showMain3(){
+        return "Well.. I dont think so...";
+    }
+
+    @GetMapping("/home/increase")
+    public int showIncrese(){
+        return count++;
+    }
+
 }
